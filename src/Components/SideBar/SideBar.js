c3e0050f7fbf0bce3267abd9ddccdb52c4a8c97d
Connor Bailey <bailey.connor.p@gmail.com>
2019-04-11 10:18:58 -0500

    sidebar and footer apear on profile page, but require styling fixes

---
+++ src/Components/SideBar/SideBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react"
+import { Link } from "react-router-dom"
+
+import "./SideBar.css"
+
+class SideBar extends Component {
+    render() {
+        return (
+            <section className="SideBar">
+                <Link className="NavLink" to="/">
+                    <button className="SideBarLink"
+                    >My Lists</button>
+                </Link>
+            </section>
+        )
+    }
+}
+
+export default SideBar
