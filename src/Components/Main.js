e07762d7fa2f166af4f580e37ab1127bd1da9c4a
Connor Bailey <bailey.connor.p@gmail.com>
2019-04-10 21:26:45 -0500

    userid collected and stored properly from auth0. navbar component created and used differently depending on whether user is logged in

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react"
 
+import NavBar from "../Components/NavBar/NavBar"
+
 export default class Main extends Component {
     render() {
         return (
             <React.Fragment>
+                <NavBar
+                {...this.props}/>
                 <p className="App-intro">
-                    Hello {this.props.name}
-                    Do you want to go to the secret area? <a href="/secret">Click Here!</a>
+                    Hello {this.props.name}<br/>
+                    Ready to start using BrackIt?
+                    {/* <a href="/secret">Click Here!</a> */}
                 </p>
                 {!this.props.auth.isAuthenticated() &&
                 <div>
